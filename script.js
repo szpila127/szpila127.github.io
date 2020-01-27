d0cc21592025bb011826863908ac566141a47c8c
Sebastian Inglot <51418629+szpila127@users.noreply.github.com>
2020-01-27 20:27:33 +0100

    Final

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const apiRoot = 'https://still-waters-18400.herokuapp.com/v1/';
-  const trelloApiRoot = 'https://still-waters-18400.herokuapp.com/v1/';
+  const trelloApiRoot = 'https://still-waters-18400.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
